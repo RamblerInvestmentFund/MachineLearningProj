720ee2cfd2e3a8e9aaada05f7a3259d0eee037a9
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2021-12-03 15:02:09 -0600

    Update __init__.py
    Datasplit for SVC

--- percentReturn/__init__.py
+++ percentReturn/__init__.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd
 import talib as ta
+import sklearn
+from sklearn.model_selection import train_test_split
 
 df = yf.download('AAPL', start='2018-01-01', end='2020-10-29')
 
+# Technicals Dataframe
 df['Percent Return'] = df['Adj Close'].pct_change() * 100
 df['Daily Range'] = df['High'] - df['Low']
 df['Simple MA'] = ta.SMA(df['Adj Close'], timeperiod=10)
@@ -27,26 +30,25 @@ def signal(x):
     elif x['Percent Return'] < 3:
         return 0
 
+
 df['Signal'] = df.apply(signal, axis=1)
-
-df[['Adj Close', 'Up Band', 'Middle Band', 'Down Band', 'Simple MA']].plot(figsize=(12,10))
-plt.show()
-
 
 print(df.head())
 
+# Plotting Bands and SMA
+# df[['Adj Close', 'Up Band', 'Middle Band', 'Down Band', 'Simple MA']].plot(figsize=(12,10))
+# plt.show()
+
+# Drop Unwanted Columns & creating arrays for SVM
 X = np.array(df.drop(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Simple SMA', 'Up Band',
                       'Middle Band', 'Down Band', 'Signal'], 1))
 Y = np.array(df['Signal'])
 
+# print(X)
+# print(Y)
 
-print(X)
-
-
-
-
-# df['Close'].plot()
-# df['Percent Return'].plot()
-
+# Splitting the data into training and testing data
+X1_train, X1_test, y1_train, y1_test = train_test_split(X, Y, test_size=0.15)
+X2_train, X1_dev, y2_train, y1_dev = train_test_split(X1_train, y1_train, test_size=0.15)
 
 
