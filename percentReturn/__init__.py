7da78ce869770a309c693dab324336a350fc05f6
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2021-12-03 13:43:22 -0600

    Update __init__.py
    BolingerBands

--- percentReturn/__init__.py
+++ percentReturn/__init__.py
@@ -7,9 +7,11 @@ import talib as ta
 df = yf.download('AAPL', start='2018-01-01', end='2020-10-29')
 
 df['Percent Return'] = df['Adj Close'].pct_change() * 100
-df['Range'] = df['High'] - df['Low']
-df['Simple MA'] = ta.SMA(df['Adj Close'], timeperiod=5)
-df['Close/SMA Difference'] = df['Adj Close'] - df['Simple MA']
+df['Daily Range'] = df['High'] - df['Low']
+df['Simple MA'] = ta.SMA(df['Adj Close'], timeperiod=10)
+df['Daily Close - SMA Difference'] = df['Adj Close'] - df['Simple MA']
+df['Up Band'], df['Middle Band'], df['Down Band'] = ta.BBANDS(df['Adj Close'], timeperiod=5)
+
 
 print(df.head())
 
