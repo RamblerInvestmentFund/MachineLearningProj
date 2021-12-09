91ebe1a35cf03d3f61d8180acfe427a8dadc7d9a
Matthew Hyatt <mhyatt@luc.edu>
2021-12-08 23:46:08 -0600

    main

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import data
-import svm
-import rnn
+from custom_models import svm
+from custom_models import rnn
 
 def main():
 
     'anything to show Dr. Dligach'
     'final print statements etc'
+
+    ## runs the svm code
+    custom_models.svm.main()
+
+    ## runs the rnn code
+    # custom_models.rnn.main()
 
 if __name__ == '__main__':
     main()
