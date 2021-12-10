a855fbf2c826cb66281cb2f4a369425e9fc0b150
Matthew Hyatt <mhyatt@luc.edu>
2021-12-10 15:27:45 -0600

    annotations

--- SVMGraphs.py
+++ SVMGraphs.py
@@ -1,45 +1,55 @@
-import yfinance as yf
-import pandas as pd
 import matplotlib.pyplot as plt
-import SVMRIF
-import Data
-import pandas as pd
 import numpy as np
+import pandas as pd
 import talib as ta
+import yfinance as yf
+from sklearn import metrics, preprocessing, svm
+from sklearn.model_selection import train_test_split
 from talib import MA_Type
 from tqdm import tqdm
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn import svm
+
+import data
+import RIFSVM
+
+'''
+the program doesnt run:
+----------
+RIFSVM' has no attribute 'simulation'
+'''
 
 
-print(len(SVMRIF.simulation(Data.preprocess('AAPL'))))
-
-
-def percentgain(gain,start):
+def percentgain(gain, start):
+    """
+    not called in project
+    """
     percentgains = []
     for i in range(5):
-        percentgains.append(BHReturn(gain[i],start[i]))
+        percentgains.append(BHReturn(gain[i], start[i]))
     return percentgains
 
 
-def model(df):
+def model(df:DataFrame) -> int:
+
+    ## build model
     model = svm.SVC(kernel="poly")
 
-    normalized_df=(df-df.mean())/df.std()
-
+    ## normalize df
+    normalized_df = (df - df.mean()) / df.std()
     normalized_df["Signal"] = df["Signal"]
-    normalized_df = normalized_df.drop(["High", "Low", "Close", "Adj Close", "Returns"], axis=1, inplace=False)
+    normalized_df = normalized_df.drop(
+        ["High", "Low", "Close", "Adj Close", "Returns"], axis=1, inplace=False
+    )
 
+    ## ??? could be this be called from RIFSVM.model(df)
     modern_predict = normalized_df.iloc[4864:]
     normalized_df = normalized_df.iloc[:-365]
     print(normalized_df)
     print(modern_predict)
 
     ModernX_test = np.array(modern_predict[modern_predict.columns[:-2]])
-    #Moderny_test = np.array(normalized_df[normalized_df.columns[-1]])
+    # Moderny_test = np.array(normalized_df[normalized_df.columns[-1]])
 
+    ## split df
     train, test = train_test_split(normalized_df, test_size=0.2)
 
     X_train = np.array(train[train.columns[:-2]])
@@ -48,12 +58,12 @@ def model(df):
     X_test = np.array(test[test.columns[:-2]])
     y_test = np.array(test[test.columns[:-1]])
 
+    ## predictions
     model.fit(X_train, y_train)
 
     y_pred = list(model.predict((ModernX_test)))
-    y_pred = [1 if i==1 else -1 for i in y_pred]
-    gain_value = list(df['Gain Value'])
-
+    y_pred = [1 if i == 1 else -1 for i in y_pred]
+    gain_value = list(df["Gain Value"])
 
     gains = [val * action for val, action in zip(gain_value, y_pred)]
     net = sum(gains)
@@ -66,4 +76,14 @@ def model(df):
 
     return sum(cumulative_gains)
 
-print(model(Data.preprocess('AAPL')))
+
+def main():
+
+    print(len(RIFSVM.simulation(data.preprocess("AAPL"))))
+
+    ## returns the sum of gains according to a trading strategy
+    print(model(data.preprocess("AAPL")))
+
+
+if __name__ == "__main__":
+    main()
