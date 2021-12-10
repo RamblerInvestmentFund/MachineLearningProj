9c457eaaaba4e923585b30467ff3bd984f6eeaff
anthonypeters <apeters10@luc.edu>
2021-12-09 22:05:05 -0600

    Did nothing.

--- data.py
+++ data.py
@@ -17,10 +17,10 @@ from sklearn import svm
 def preprocess(ticker):
     """returns a pandas dataframe with technical indicators"""
 
-    ## download dataset
+    ## Download dataset
     df = yf.download(tickers=ticker, start="2001-01-01")
 
-    ## technical indicators
+    ## Technical indicators
     df["High Shifted"] = df["High"].shift(1)
     df["Low Shifted"] = df["Low"].shift(1)
     df["Close Shifted"] = df["Close"].shift(1)
