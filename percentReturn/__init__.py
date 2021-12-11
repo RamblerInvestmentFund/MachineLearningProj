4b5a98997cae69e754a41a689319ee7e0b7c7795
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2021-12-10 20:49:26 -0600

    Update __init__.py
    Implemented SVM predicting <2% & > 2% daily price change

--- percentReturn/__init__.py
+++ percentReturn/__init__.py
@@ -4,9 +4,13 @@ import yfinance as yf
 import pandas as pd
 import talib as ta
 import sklearn
+from sklearn.dummy import DummyClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
 
-df = yf.download('AAPL', start='2018-01-01', end='2020-10-29')
+df = yf.download('SPY', start='2009-01-01', end='2020-10-29')
 
 # Technicals Dataframe
 df['Percent Return'] = df['Adj Close'].pct_change() * 100
@@ -25,30 +29,52 @@ df['Daily Close - Low'] = abs(df['Adj Close'] - df['Low'])
 
 
 def signal(x):
-    if x['Percent Return'] > 3:
+    if x['Percent Return'] > 2:
         return 1
-    elif x['Percent Return'] < 3:
+    elif x['Percent Return'] < -2:
+        return -1
+    else:
         return 0
 
 
 df['Signal'] = df.apply(signal, axis=1)
 
+# Remove first 9 rows bc data is not complete; Contains NaN while calculating percent return, etc.
+df = df.iloc[9:]
 print(df.head())
 
 # Plotting Bands and SMA
-# df[['Adj Close', 'Up Band', 'Middle Band', 'Down Band', 'Simple MA']].plot(figsize=(12,10))
-# plt.show()
+df[['Adj Close', 'Up Band', 'Middle Band', 'Down Band', 'Simple MA']].plot(figsize=(12,10))
+plt.show()
 
 # Drop Unwanted Columns & creating arrays for SVM
-X = np.array(df.drop(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Simple SMA', 'Up Band',
-                      'Middle Band', 'Down Band', 'Signal'], 1))
+X = np.array(df.drop(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Simple MA', 'Up Band',
+                      'Middle Band', 'Down Band', 'Signal'], axis = 1))
+# Dropping Date Column
+X = X[:, 1:]
+
 Y = np.array(df['Signal'])
 
-# print(X)
-# print(Y)
+# Normalizing Data
+X_max = X.max()
+X_min = X.min()
+X_range = X_max - X_min
+X_norm = (X - X_min) / X_range
 
-# Splitting the data into training and testing data
-X1_train, X1_test, y1_train, y1_test = train_test_split(X, Y, test_size=0.15)
-X2_train, X1_dev, y2_train, y1_dev = train_test_split(X1_train, y1_train, test_size=0.15)
+#DataSplit
+X_train, X_test, y_train, y_test = train_test_split(X_norm, Y, test_size=.15, random_state=20)
+X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size=.15, random_state=20)
 
+# Find Best Parameters
+param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['rbf', 'linear']}
+grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=4)
+grid.fit(X_train, y_train)
+print(grid.best_params_)
 
+# SVC With Best Parameters
+grid_pred = grid.predict(X_dev)
+cmatrix = np.array(confusion_matrix(y_dev, grid_pred, labels = [1, -1]))
+confusion_best = pd.DataFrame(cmatrix, index = ['Greater than 2percent', 'Less than -2percent'],
+                              columns = ['Predicted Greater than 2percent', 'Predicted Less than -2percent'])
+print(confusion_best)
+print(classification_report(y_dev, grid_pred))
