ae08caa52ca98266e80397ffce2914eda883b984
anthonypeters <apeters10@luc.edu>
2021-12-15 18:09:46 -0600

    Annoted final doc

--- finalWholeMain.py
+++ finalWholeMain.py
@@ -7,11 +7,11 @@ from talib import MA_Type
 
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn import svm
+from sklearn.linear_model import LogisticRegression
 
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
@@ -20,9 +20,15 @@ import matplotlib.ticker as ticker
 def signal(x):
     return 1 if x['Returns'] >= 0 else 0
 
-max_abs_scaler = preprocessing.MaxAbsScaler()
-stocks = ["AAPL"] #, "MSFT", "SPY", "QQQ", "DIA", "TLT", "GLD", "CVX", "KO", "PEP", "PG", "JNJ", "GSK"]
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+'''
+
+Downloads Data, creates Dataframe, and splits into train/test
+
+'''
+
+stocks = ["AAPL"] #, "MSFT", "SPY", "QQQ", "DIA", "TLT", "GLD", "CVX", "KO", "PEP", "PG", "JNJ", "GSK"]
 frames = []
 for i in stocks:
 
@@ -42,25 +48,20 @@ for i in stocks:
     df = df[40:]
     frames.append(df)
 
-
-X = np.array(frames[0].drop(columns = ["Signal", "Returns"], axis = 1))
+max_abs_scaler = preprocessing.MaxAbsScaler()
+X = np.array(frames[0].drop(columns = ["Signal", "Returns", "Open", "High", "Low", "Close", "Adj Close", "High Shifted", "Low Shifted", "Close Shifted"], axis = 1))
 X = max_abs_scaler.fit_transform(X)
 Y = np.array(frames[0]["Signal"])
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30)
 
-model = svm.SVC(kernel="rbf", C=1000, gamma=1)
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-print(classification_report(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
-
-
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 '''
 
-Grid Search
-After Running Grid Search, it found rbf, C=1000, and gamma=1 as best hyperparameters
+Grid Search:
+
+SVM: After Running Grid Search, it found rbf, C=1000, and gamma=1 as best hyperparameters
+LogisticRegression: After running Grid search it found C=100 and penalty=l2
 
 '''
 
@@ -73,50 +74,97 @@ After Running Grid Search, it found rbf, C=1000, and gamma=1 as best hyperparame
 # print(grid.best_params_)
 # print(grid.best_estimator_)
 
+# grid_values = {'penalty': ['l2'], 'C': [0.001,0.01,0.1,1,10,100,1000]}
+# model_lr = GridSearchCV(LogisticRegression(), param_grid=grid_values)
+# model_lr.fit(X_train, y_train)
+# print(model_lr.best_params_)
+# print(model_lr.best_estimator_)
 
-pred_len = len(y_pred)
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-frames[0]['SVM Signal'] = 0
-frames[0]['SVM Returns'] = 0
-frames[0]['Total Strat Returns'] = 0
-frames[0]['Market Returns'] = 0
+'''
 
-Signal_Column = frames[0].columns.get_loc('SVM Signal')
-Strat_Column = frames[0].columns.get_loc('SVM Returns')
-Return_Column = frames[0].columns.get_loc('Total Strat Returns')
-Market_Column = frames[0].columns.get_loc('Market Returns')
+Running Both SVM and Logisitic Regression with optimized hyperparamters
 
-frames[0].iloc[-pred_len:,Signal_Column] = list(map(int,y_pred))
-frames[0]['SVM Returns'] = frames[0]['SVM Signal'] * frames[0]['Returns'].shift(1)
+'''
 
-frames[0].iloc[-pred_len:,Return_Column] = np.nancumsum(frames[0]['SVM Returns'][-pred_len:])
-frames[0].iloc[-pred_len:,Market_Column] = np.nancumsum(frames[0]['Returns'][-pred_len:])
+model = svm.SVC(kernel="rbf", C=1000, gamma=1)
+model.fit(X_train, y_train)
 
-sharpe_ratio = (frames[0]['Total Strat Returns'][-1] - frames[0]['Market Returns'][-1])/ \
-                    np.nanstd(frames[0]['Total Strat Returns'][-pred_len:])
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
 
+# model = LogisticRegression(C=100, penalty='l2')
+# model.fit(X_train, y_train)
 
-print(sharpe_ratio)
+# y_pred = model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
 
+'''
 
-fig, ax = plt.subplots(figsize=(9, 7))
+Logistic Regression
+Accuracy: 0.62
+Precision: 0:0.59 1:0.63 Avg:0.61
+Recall: 0:0.47 1:0.74 Avg: 0.62
+f1-score: 0:0.52 1:0.68 Avg: 0.61
 
-ax.plot(frames[0][-pred_len:].index.values,
-        frames[0]['Total Strat Returns'][-pred_len:].values, color='g', label="Strat Returns")
+'''
 
-ax.plot(frames[0][-pred_len:].index.values,
-        frames[0]['Market Returns'][-pred_len:].values, color='b', label="Market Returns")
+'''
 
-ax.set(xlabel= "Date",ylabel="Returns")
-plt.title(i,fontsize=15)
-ax.xaxis.set_major_locator(ticker.AutoLocator())
+SVM
+Accuracy: 0.63
+Precision: 0:0.61 1:0.64 Avg:0.63
+Recall: 0:0.5 1:0.74 Avg:0.63
+f1-score: 0:0.55 1:0.69 Avg:0.62
 
-# plt.figtext(.95,0.78, s="Sharpe Ratio "+'{0:.5g}'.format(Model_Dict[i]['Sharpe_Ratio']))
-#plt.figtext(.95,0.75, s="Sum Total Strat Returns "+'{0:.5g}'.format(frames[0]['Total Strat Returns'].sum()))
+'''
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-plt.legend(loc='best')
-plt.savefig("AAPL.png")
-plt.show()
-# plt.figtext(.95,0.72,s="Model Accuracy " +'{0:.5g}'.format(Model_Dict[i]['Accuracy']))
-# plt.figtext(.95,0.69,s="Model Precision " +'{0:.5g}'.format(Model_Dict[i]['Precision']))
-# plt.figtext(.95,0.66,s="Model Recall " +'{0:.5g}'.format(Model_Dict[i]['Recall']))
+'''
+
+Graphing Buy and Hold returns against our SVM signal Returns
+Computing Sharpe Ratio as well
+
+'''
+
+# pred_len = len(y_pred)
+
+# frames[0]['SVM Signal'] = 0
+# frames[0]['SVM Returns'] = 0
+# frames[0]['Total Strat Returns'] = 0
+# frames[0]['Market Returns'] = 0
+
+# Signal_Column = frames[0].columns.get_loc('SVM Signal')
+# Strat_Column = frames[0].columns.get_loc('SVM Returns')
+# Return_Column = frames[0].columns.get_loc('Total Strat Returns')
+# Market_Column = frames[0].columns.get_loc('Market Returns')
+
+# frames[0].iloc[-pred_len:,Signal_Column] = list(map(int,y_pred))
+# frames[0]['SVM Returns'] = frames[0]['SVM Signal'] * frames[0]['Returns'].shift(1)
+
+# frames[0].iloc[-pred_len:,Return_Column] = np.nancumsum(frames[0]['SVM Returns'][-pred_len:])
+# frames[0].iloc[-pred_len:,Market_Column] = np.nancumsum(frames[0]['Returns'][-pred_len:])
+
+# sharpe_ratio = (frames[0]['Total Strat Returns'][-1] - frames[0]['Market Returns'][-1])/ \
+#                     np.nanstd(frames[0]['Total Strat Returns'][-pred_len:])
+
+# print(sharpe_ratio)
+
+# fig, ax = plt.subplots(figsize=(9, 7))
+
+# ax.plot(frames[0][-pred_len:].index.values,
+#         frames[0]['Total Strat Returns'][-pred_len:].values, color='g', label="Strat Returns")
+
+# ax.plot(frames[0][-pred_len:].index.values,
+#         frames[0]['Market Returns'][-pred_len:].values, color='b', label="Market Returns")
+
+# ax.set(xlabel= "Date",ylabel="Returns")
+# plt.title(i,fontsize=15)
+# ax.xaxis.set_major_locator(ticker.AutoLocator())
+
+# plt.legend(loc='best')
+# plt.savefig("AAPL.png")
+# plt.show()
